845906632487a4780737df32476fb4318fa8d081
NhanLe2426 <lethanhnhan0401@gmail.com>
2025-09-17 22:16:38 +0700

    Init for Exercise6

---
+++ STM32_MCU_Lab1/Core/Inc/exercise6.h
@@ -0,0 +1,19 @@
+/*
+ * exercise6.h
+ *
+ *  Created on: Sep 17, 2025
+ *      Author: ASUS
+ */
+
+#ifndef INC_EXERCISE6_H_
+#define INC_EXERCISE6_H_
+
+/* Includes */
+#include "main.h"
+
+/* Variables */
+
+/* Functions */
+extern void displayLED(int num);
+
+#endif /* INC_EXERCISE6_H_ */
